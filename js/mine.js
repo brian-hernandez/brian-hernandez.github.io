762f3a4fd32df4e8b3fd1603c14b8f4792768bcf
Brian-Hernandez <mad_cap111@mac.com>
2016-08-28 22:56:46 -0700

    added new image and button action

--- js/mine.js
+++ js/mine.js
@@ -2,7 +2,10 @@ function lights() {
     var checkbox = document.getElementById("lights");
     var body = document.getElementById("mybody");
     var nav = document.getElementById("mynav");
+    var img = document.getElementById("ci");
+    var img2 = document.getElementById("ci2");
 
+    console.log(img);
     var currNav = nav.className;
     console.log(currNav);
 
@@ -11,10 +14,18 @@ function lights() {
         body.style["color"] = "white";
         body.style["background"] = "black";
         nav.className = "navbar navbar-inverse navbar-fixed-top";
+        img.src = "LA.jpg";
+        img2.src = "LA.jpg";
     } else {
         console.log("Its not checked");
         body.style["color"] = "black";
         body.style["background"] = "white";
         nav.className = "navbar navbar-default navbar-fixed-top";
+        img.src = "city-630101_1920.jpg";
+        img2.src = "city-630101_1920.jpg";
     }
+}
+
+function alertmMe(){
+    alert("Hey");
 }
