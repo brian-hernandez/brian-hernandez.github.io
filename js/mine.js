080ad5139d60ba493f3a5990b16e8a453ce56de8
Brian-Hernandez <mad_cap111@mac.com>
2016-08-29 14:08:08 -0700

    refactored some code and organized directory

--- js/mine.js
+++ js/mine.js
@@ -1,3 +1,10 @@
+/*
+ The lights definition
+
+ This function changes the page theme to a 'Dark Mode' and 'Light Mode'
+
+ */
+
 function lights() {
     var checkbox = document.getElementById("lights");
     var body = document.getElementById("mybody");
@@ -16,47 +23,61 @@ function lights() {
         body.style["background"] = "black";
         jumbo.style["background"] = "#0D0D0D";
         nav.className = "navbar navbar-inverse navbar-fixed-top";
-        img.src = "LA.jpg";
-        img2.src = "LA.jpg";
+        img.src = "img/NY.jpg";
+        img2.src = "img/LA2.jpg";
     } else {
         console.log("Its not checked");
         body.style["color"] = "black";
         body.style["background"] = "white";
         jumbo.style["background"] = "#F2F2F2";
         nav.className = "navbar navbar-default navbar-fixed-top";
-        img.src = "city-630101_1920.jpg";
-        img2.src = "city-630101_1920.jpg";
+        img.src = "img/LA2.jpg";
+        img2.src = "img/NY.jpg";
     }
 }
 
-function alertmMe() {
-    alert("Hey");
+/*
+ The Choice definition
+
+ @param {String} id - The ID of the selected tag.
+
+ This function sets the style.display of the 'inactive' articles
+ to 'none' and the 'active' article to 'block.
+ */
+var Choice = function (id) {
+
+    var art1 = document.getElementById("art1");
+    var art2 = document.getElementById("art2");
+    var art3 = document.getElementById("art3");
+
+    if (id == "a1") {
+        art1.style["display"] = "block";
+        art2.style["display"] = "none";
+        art3.style["display"] = "none";
+    } else if (id == "a2") {
+        art1.style["display"] = "none";
+        art2.style["display"] = "block";
+        art3.style["display"] = "none";
+    } else {
+        art1.style["display"] = "none";
+        art2.style["display"] = "none";
+        art3.style["display"] = "block";
+    }
 }
 
+/*
+ Function that checks for which group-item was clicked on
+ and runs Choice function to display corresponding article
+ * Makes use of JQuery *
+ */
 $(function () {
     $('.list-group a').click(function (e) {
-        e.preventDefault()
+        e.preventDefault();
         $that = $(this);
-        console.log($that);
-        var art1 = document.getElementById("art1");
-        var art2 = document.getElementById("art2");
-        var art3 = document.getElementById("art3");
-
-        if (this.id == "a1") {
-            art1.style["display"] = "block";
-            art2.style["display"] = "none";
-            art3.style["display"] = "none";
-        } else if (this.id == "a2") {
-            art1.style["display"] = "none";
-            art2.style["display"] = "block";
-            art3.style["display"] = "none";
-        } else {
-            art1.style["display"] = "none";
-            art2.style["display"] = "none";
-            art3.style["display"] = "block";
-        }
-
+        Choice(this.id);
         $that.parent().find('a').removeClass('active');
         $that.addClass('active');
+
+        console.log(e.result);
     });
 })
