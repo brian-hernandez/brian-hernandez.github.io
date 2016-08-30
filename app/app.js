ef8e293b43b86ef8052ecd0ad3175f7cb6557e2c
Brian-Hernandez <mad_cap111@mac.com>
2016-08-30 14:37:52 -0700

    angular attempt

--- app/app.js
+++ app/app.js
@@ -1,15 +1,16 @@
 /**
  * Created by Brian on 8/29/16.
  */
-var myPortfolio = angular.module('myPortfolio', ['ngRoute']);
+var myPortfolio = angular.module('myPortfolio', ['ngRoute', 'home']);
 
 myPortfolio.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/home', {
-            templateUrl: 'homeView.html',
+            templateUrl: '../Home/homeView.html',
             controller: 'homeCtrl'
         }).otherwise({
         templateUrl: 'homeView.html',
         controller: 'homeCtrl'
     })
+
 }]);
--- app/components/Home/homeController.js
+++ app/components/Home/homeController.js
@@ -1,3 +1,9 @@
 /**
  * Created by Brian on 8/29/16.
  */
+var home = angular.module('home', []);
+
+
+home.controller('homeCtrl', function homeControler($scope) {
+    console.log("Hello you've reached the home controller");
+});
