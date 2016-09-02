647ad9f794b9c1a33e7df4b70446d82d290b858b
Brian-Hernandez <mad_cap111@mac.com>
2016-09-02 13:06:21 -0700

    fo

--- app/components/Home/homeController.js
+++ app/components/Home/homeController.js
@@ -1,9 +1,13 @@
 /**
  * Created by Brian on 8/29/16.
  */
-var home = angular.module('home', []);
+(function () {
+    var home = angular.module('home', []);
 
-
-home.controller('homeCtrl', function homeControler($scope) {
-    console.log("Hello you've reached the home controller");
-});
+    home.controller('QuestionsController', function () {
+        console.log("controller initialized");
+        this.addPoll = function () {
+            console.log("inside function");
+        };
+    });
+})();
