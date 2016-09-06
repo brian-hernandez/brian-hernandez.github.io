9313f1811f6264221c62690c196dd6b4a72756f3
Brian-Hernandez <mad_cap111@mac.com>
2016-09-05 18:49:11 -0700

    abb

--- js/abb.js
+++ js/abb.js
@@ -3,5 +3,6 @@ $.getJSON('../drummers.json', function (data) {
         var imgs = "<div class='col-md-3'><a target='_blank' class='thumbnail darken' href='" + f.yt_url + "'><img" +
             " class='img-responsive' src='" + f.pic_url + "'></a></div>";
         $(imgs).appendTo(".gri");
+        console.log(imgs);
     })
 });
