1fc9fb8304ccbc089b5834b88c80475c34f2db79
Brian-Hernandez <mad_cap111@mac.com>
2016-09-06 00:14:00 -0700

    designing new home page

---
+++ js/home.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Brian on 9/5/16.
+ */
+
+
+
+$(window).scroll(function () {
+
+    console.log($(window).scrollTop());
+});
+
+
+$(document).ready(function () {
+    var scroll = 0;
+    var startAt = $('.s2');
+    var offset = startAt.offset();
+    if (startAt.length) {
+        $(document).scroll(function () {
+            startAt = $(this).scrollTop();
+            if (startAt > offset.top) {
+                $(".navbar-default").css('background-color', '#462521')
+                //document.getElementById("navL").className = "fa fa-instagram fa-2x FA";
+            } else {
+                $(".navbar-default").css('background-color', 'transparent')
+                //document.getElementById("navL").className = "";
+            }
+        })
+    }
+});
+
