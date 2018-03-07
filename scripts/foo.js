1f63c98160f8489aac90c4c19cc30f487910c80a
Brian-Hernandez <mad_cap111@mac.com>
2018-03-06 19:33:32 -0800

    More tests

--- scripts/foo.js
+++ scripts/foo.js
@@ -2,5 +2,15 @@ var $ = require('jquery');
 window.$ = $;
 require('bootstrap');
 
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+
 // Will be true if bootstrap is loaded, false otherwise
 var bootstrap_enabled = (typeof $().modal == 'function');
